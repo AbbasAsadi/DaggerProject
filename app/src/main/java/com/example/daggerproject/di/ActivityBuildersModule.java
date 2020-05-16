cb397ec797357135ce56b6293b9d5ae7b37183e7
abbasasadi <abbas.asadi1996@gmail.com>
2020-05-16 15:11:12 +0430

    Add Scope

--- app/src/main/java/com/example/daggerproject/di/ActivityBuildersModule.java
+++ app/src/main/java/com/example/daggerproject/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.example.daggerproject.di;
 
 import com.example.daggerproject.di.auth.AuthModule;
+import com.example.daggerproject.di.auth.AuthScope;
 import com.example.daggerproject.di.auth.AuthViewModelModule;
 import com.example.daggerproject.di.main.MainFragmentBuilderModule;
 import com.example.daggerproject.di.main.MainModule;
+import com.example.daggerproject.di.main.MainScope;
 import com.example.daggerproject.di.main.MainViewModelModule;
 import com.example.daggerproject.ui.auth.AuthActivity;
 import com.example.daggerproject.ui.main.MainActivity;
@@ -13,12 +15,14 @@ import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class ActivityBuildersModule {
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {AuthViewModelModule.class,
                     AuthModule.class}
     )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {MainFragmentBuilderModule.class , MainViewModelModule.class , MainModule.class}
     )
--- app/src/main/java/com/example/daggerproject/ui/main/MainActivity.java
+++ app/src/main/java/com/example/daggerproject/ui/main/MainActivity.java
@@ -6,26 +6,37 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.daggerproject.BaseActivity;
 import com.example.daggerproject.R;
 import com.example.daggerproject.ui.main.posts.PostFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
 
-        testFragment();
+        init();
     }
 
-    private void testFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container, new PostFragment())
-                .commit();
+    private void init() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mDrawerLayout);
+        NavigationUI.setupWithNavController(mNavigationView, navController);
+        mNavigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -45,6 +56,26 @@ public class MainActivity extends BaseActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_profile:
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.profileScreen);
+                break;
+            case R.id.nav_posts:
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.postsScreen);
+                break;
+        }
+        item.setChecked(true);
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
 
